0145ec7e995eb1ae3c098960c50ae0e0b9b461e7
Petro Verkhogliad <vpetro@gmail.com>
2022-02-07 23:46:59 -0500

    Configure null-ls for Python and Lua
    * black for python formatting
    * flake8 for python diagnostics
    * stylua for lua formatting

--- lua/vpetro/init.lua
+++ lua/vpetro/init.lua
@@ -5,3 +5,5 @@ require'vpetro.treesitter'
 require'vpetro.lsp.lua'
 require'vpetro.lsp.python'
 require'vpetro.lsp.haskell'
+
+require'vpetro.lsp.null'
---
+++ lua/vpetro/lsp/null.lua
@@ -0,0 +1,13 @@
+local null_ls = require"null-ls"
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+    sources = {
+        formatting.stylua.with {
+          extra_args = { "--indent-type Spaces", "--indent-width 2"}
+        },
+        formatting.black.with { extra_args = { "--fast" } },
+        diagnostics.flake8
+    },
+})
