856f9e7b990615d5dedefc0c45eef7976cd2c779
Petro Verkhogliad <vpetro@gmail.com>
2023-04-29 19:02:22 -0400

    Remove replaced/outdated/moved files

--- lua/vpetro/lsp/python.lua
+++
@@ -1,9 +0,0 @@
-local lh = require("vpetro.lsp_helper")
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-require'lspconfig'.jedi_language_server.setup{ capabilities = capabilities }
-
-lh.setup_keymaps()
-lh.setup_diagnostics()
--- lua/vpetro/snippets.lua
+++
@@ -1,9 +0,0 @@
-local ls = require'luasnip'
-local s = ls.snippet
-local t = ls.text_node
-
-ls.snippets = {
-  all = {
-    s("tgg", t("Wow! Text!")),
-  }
-}
