a4b0b46e8b9a01f899443755557dca56393d4994
Petro Verkhogliad <vpetro@gmail.com>
2025-11-05 15:18:47 -0500

    Experiment with more treesitter objects

--- lua/vpetro/treesitter.lua
+++ lua/vpetro/treesitter.lua
@@ -1,8 +1,65 @@
-local treesitter = require('nvim-treesitter.configs')
+local tt = require('nvim-treesitter.configs')
 
-treesitter.setup {
-  ensure_installed = {"lua", "python", "java", "scala"},
+tt.setup {
+  -- "query" is necessary in order to make the query editor work in playground
+  ensure_installed = {"markdown", "lua", "vimdoc", "python", "java", "scala", "json", "yaml", "query", "javascript", "typescript", "rust", "ocaml", "haskell"},
+  indent = {
+    enable = true,
+    -- disable = {"python", "yaml"},
+  },
   highlight = {
-      enable = true
-  }
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",  -- start selection
+      node_incremental = "grn", -- expand selection 
+      scope_incremental = "grc", -- expand selection
+      node_decremental = "grm" -- shrink selection
+    }
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    swap = {
+      enable = false,
+    },
+    lsp_interop = {
+      enable = true,
+      border = 'none',
+      floating_preview_opts = {},
+      peek_definition_code = {
+        ["<leader>df"] = "@function.outer",
+        ["<leader>dF"] = "@class.outer",
+      },
+    },
+  },
 }
