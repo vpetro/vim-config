cd9a1e85c8eb5b957be99a682d33d2749f703d5e
Petro Verkhogliad <vpetro@gmail.com>
2026-08-14 22:51:59 -0400

    Trim the gopls analyzer list
    Drop names that are not gopls analyzers (deba, contextcheck, composite)
    and the block of explicit falses, which was silently disabling most of
    the default vet suite (copylocks, loopclosure, lostcancel, printf-family
    checks) under a "less commonly needed" label. Keep the deliberate
    enables plus the shadow/printf/structtag opt-outs.

--- lsp/go.lua
+++ lsp/go.lua
@@ -25,80 +25,18 @@ return {
         rangeVariableTypes = false,
       },
       analyses = {
-        -- NOTE: To temporarily enable disabled analyzers for specific debugging:
-        -- :lua vim.lsp.stop_client(vim.lsp.get_clients({name = "gopls"}))
-        -- Then edit this file and save, LSP will restart with new settings
-
-        -- Essential analyzers for catching common issues
-        nilness = true,              -- Check for nil pointer dereferences
-        unusedparams = true,         -- Find unused function parameters
-        unusedwrite = true,          -- Find unused writes to variables
-        useany = true,               -- Suggest using 'any' instead of 'interface{}'
-        unreachable = true,          -- Find unreachable code
-        unusedresult = true,         -- Check for unused results of calls to certain functions
-
-        -- Helpful but not critical (enable as needed)
-        simplifyslice = true,                -- Simplify slice expressions
-        simplifyrange = true,                -- Simplify range loops
-        simplifycompositelit = true,         -- Simplify composite literals
-
-        -- Performance-intensive analyzers (disabled for better performance)
-        shadow = false,            -- Check for shadowed variables (can be slow)
-        printf = false,            -- Check printf-style functions (can be slow)
-        structtag = false,         -- Check struct tags (can be slow)
-        -- fieldalignment = false,  -- Check struct field alignment (very slow)
-        -- unusedvariable = false,  -- Can be slow on large codebases
-
-        -- Less commonly needed analyzers (disabled)
-        modernize = false,
-        stylecheck = false,
-        appends = false,
-        asmdecl = false,
-        assign = false,
-        atomic = false,
-        atomicalign = false,
-        bools = false,
-        buildtag = false,
-        cgocall = false,
-        composite = false,
-        composites = false,
-        contextcheck = false,
-        copylocks = false,
-        deba = false,
-        deepequalerrors = false,
-        defers = false,
-        deprecated = false,
-        directive = false,
-        embed = false,
-        errorsas = false,
-        fillreturns = false,
-        framepointer = false,
-        gofix = false,
-        hostport = false,
-        httpresponse = false,
-        ifaceassert = false,
-        infertypeargs = false,
-        loopclosure = false,
-        lostcancel = false,
-        nilfunc = false,
-        nonewvars = false,
-        noresultvalues = false,
-        shift = false,
-        sigchanyzer = false,
-        slog = false,
-        sortslice = false,
-        stdmethods = false,
-        stdversion = false,
-        stringintconv = false,
-        testinggoroutine = false,
-        tests = false,
-        timeformat = false,
-        unmarshal = false,
-        unsafeptr = false,
-        unusedfunc = false,
-        unusedvariable = false,
-        waitgroup = false,
-        yield = false,
+        nilness = true,
+        unusedparams = true,
+        unusedwrite = true,
+        useany = true,
+        unreachable = true,
+        unusedresult = true,
+        simplifyslice = true,
+        simplifyrange = true,
+        simplifycompositelit = true,
+        shadow = false,
+        printf = false,
+        structtag = false,
       },
       usePlaceholders = true,
       completeUnimported = true,
