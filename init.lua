db33a03003413a0f65d5b2ad7f18c3e6e88a8b65
Petro Verkhogliad <vpetro@gmail.com>
2026-08-24 02:38:08 -0400

    Pin blink.cmp to the v1 release line

--- init.lua
+++ init.lua
@@ -32,7 +32,7 @@ vim.pack.add({
   { src = 'https://github.com/ibhagwan/fzf-lua' },
 
   -- completion & snippets
-  { src = 'https://github.com/saghen/blink.cmp' },
+  { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.*') },
 
   { src = "https://github.com/iamcco/markdown-preview.nvim" },
 
